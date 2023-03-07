6c126ca870808081714498edafb56e929e8e01a4
uamanager <faradey.ivan@gmail.com>
2023-03-07 16:37:59 +0200

    ci: nx scopes linter
    add nx scopes linter
    
    Signed-off-by: uamanager <faradey.ivan@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  extends: [
-    '@commitlint/config-conventional',
-    '@commitlint/config-conventional',
-  ],
+  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
 };
